b9bce4608cee9f2a67a6b48ad5adc3d014f57141
AkmalMakh <akmal.makhmudov57@gmail.com>
2021-01-26 21:31:49 +0900

    applied to sign in screen

--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text} from 'react-native';
+import AuthContext from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const  SigninScreen = () =>{
-    return <Text style={{fontSize: 48}}>
-      SigninScreen
-    </Text>
-}
+  //const {state ,signin} = useContext(AuthContext)
+    return (
+    <View style={styles.container}>
+        <AuthForm 
+        headerText = "Sign in to your Account"
+        errorMessage = {""}
+        submitButtonText = "Sign In"
+        onSubmit = {()=>{}}
+        
+        />
+        <NavLink
+        routeName = "Signup"
+        text = "Dont have an account? Sign up intead?"
+        />
 
-const styles = StyleSheet.create({});
+    </View>
+    );
+}
+SigninScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 230
+
+  },
+});
 
 export default  SigninScreen;
